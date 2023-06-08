ab0b359257e6b771e43a038d59d14cb4d8da66fb
+ <+>
2023-06-08 20:06:07 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(a,b){
+  return a+b;  
+}
+console.log(add(a,b))
+
+function subtract(b,a){
+    return b-a;
+}
+console.log(subtract(b-a))
+
+function multiply(a,b){
+    return a*b;
+    
+}
+console.log(multiply(a*b))
+
+function divide(a,b){
+    return a/b;
+}
+console.log(divide(a/b))
+
+function increment(a){
+    return a + 1;
+}
+console.log(increment(n))
+
+function decrement(a){
+    return a - 1;
+}
+console.log(decrement(n))
+
+function makeInt(n){
+    return parseInt(n ,10);
+}
+console.log(makeInt("n"))
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+console.log(preserveDecimal("2.222"))
+
+ function preserveDecimal(n){
+    return parseFloat();
+ } 
+ console.log(preserveDecimal("n"))
